13ca79740638779dd0277742b2f5f945d985f2a2
root <root@172.31.1.31>
2015-09-15 04:52:37 +0000

    3rd commit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-  echo "NEW! Hello world"; 
+
+echo "<h1>Hello world</h1>";
+$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+if ($mysqli->connect_error) {
+    exit($mysqli->connect_error);
+} else {
+    $mysqli->set_charset("utf8");
+}
+$result = $mysqli->query("select 1") or exit($mysqli->error());
+echo var_dump($result->fetch_assoc());
+$mysqli->close();
+
 ?>
